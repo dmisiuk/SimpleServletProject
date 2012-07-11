926b4751066173a4c681b2541e7a784b810f3c0f
Dzmitry Misiuk <minsler.by@gmail.com>
2012-07-12 00:37:59 +0300

    Added ServletContext object

--- src/by/minsler/web/SimpleSessionServlet.java
+++ src/by/minsler/web/SimpleSessionServlet.java
@@ -3,6 +3,7 @@ package by.minsler.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,13 +24,17 @@ public class SimpleSessionServlet extends HttpServlet {
 		out.println("<h1> Simple session servlet<h1>");
 		String userName = request.getParameter("userName");
 		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
 		if (userName != null && userName != ""
 				&& session.getAttribute("savedUserName") == null) {
 			session.setAttribute("savedUserName", userName);
+			context.setAttribute("savedUserName", userName);
 		}
 		out.println("User name from request: " + userName + "<br/>");
 		out.println("User name from session: "
 				+ (String) session.getAttribute("savedUserName") + "<br/>");
+		out.println("User name from servletContext: "
+				+ (String) context.getAttribute("savedUserName") + "<br/>");
 
 	}
 
